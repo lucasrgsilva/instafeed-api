61965baf1aad0300b9087cfe53eb326624556903
Lucas Rodrigues Silva <lucas.silva@caseimagine.com>
2017-12-10 01:12:20 -0200

    .

--- index.js
+++ index.js
@@ -12,50 +12,55 @@ const CLIENT_SECRET = 'add27729a01b41bead3d93da09581881';
 
 let tagVar;
 
-
-app.use(route.get('/api/tag', async (ctx) => {
+app.use((ctx) => {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    ctx.status = 400;
-    ctx.body = { 'Message': 'No tag specified' };
-}));
+    ctx.body = { 'Message': 'Successfuly started' };
+});
 
-app.use(route.get('/api/tag/:tag', async (ctx, tag) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+// app.use(route.get('/api/tag', async (ctx) => {
+//     ctx.set('Access-Control-Allow-Origin', '*');
+//     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     ctx.status = 400;
+//     ctx.body = { 'Message': 'No tag specified' };
+// }));
 
-    tagVar = tag;
-    let url = INSTAGRAM_URL + `oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=public_content`;
+// app.use(route.get('/api/tag/:tag', async (ctx, tag) => {
+//     ctx.set('Access-Control-Allow-Origin', '*');
+//     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    ctx.redirect(url);
-}));
+//     tagVar = tag;
+//     let url = INSTAGRAM_URL + `oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=public_content`;
 
-app.use(route.get('/api/auth/done', async (ctx, tag) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     ctx.redirect(url);
+// }));
 
-    let params = ctx.request.query;
+// app.use(route.get('/api/auth/done', async (ctx, tag) => {
+//     ctx.set('Access-Control-Allow-Origin', '*');
+//     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+//     let params = ctx.request.query;
     
-    let auth_user = await request({
-        uri: INSTAGRAM_URL + 'oauth/access_token/',
-        method: 'POST',
-        form: {
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            redirect_uri: REDIRECT_URI,
-            code: params.code
-        },
-        json: true
-    });
+//     let auth_user = await request({
+//         uri: INSTAGRAM_URL + 'oauth/access_token/',
+//         method: 'POST',
+//         form: {
+//             client_id: CLIENT_ID,
+//             client_secret: CLIENT_SECRET,
+//             grant_type: 'authorization_code',
+//             redirect_uri: REDIRECT_URI,
+//             code: params.code
+//         },
+//         json: true
+//     });
 
-    let photos = await request({
-        url: INSTAGRAM_URL + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
-        method: 'GET',
-        json: true
-    });
+//     let photos = await request({
+//         url: INSTAGRAM_URL + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
+//         method: 'GET',
+//         json: true
+//     });
 
-    ctx.body = { message: 'Successfully Authenticated', response: photos.data };
-}));
+//     ctx.body = { message: 'Successfully Authenticated', response: photos.data };
+// }));
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
