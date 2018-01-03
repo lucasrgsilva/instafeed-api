37f3794726bc5f8e47c46000638136dac408601c
Lucas Rodrigues Silva <lucas.silva@caseimagine.com>
2018-01-03 14:14:13 -0200

    .

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 const PORT = process.env.PORT || 5000
 // const REDIRECT_URI = 'http://localhost:5000/api/auth/done';
 // const REDIRECT_URI = 'https://instagallery-api.herokuapp.com/api/auth/done';
-const INSTAGRAM_URL = 'https://api.instagram.com/';
+const INSTAGRAM_API = 'https://api.instagram.com/';
 const CLIENT_ID = '20d1ab5af77445d9b09a46eaa6e3bb0c';
 const CLIENT_SECRET = 'add27729a01b41bead3d93da09581881';
 
@@ -45,12 +45,10 @@ app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag) => {
     const access_token = await client.getAsync(user);
     const code = ctx.request.query.code;
 
-    // ctx.body = { response: ctx.request.header.referer};
-
     if (access_token) {
 
         let photos = await request({
-            url: INSTAGRAM_URL + `v1/tags/${tag}/media/recent?access_token=${access_token}`,
+            url: INSTAGRAM_API + `v1/tags/${tag}/media/recent?access_token=${access_token}`,
             method: 'GET',
             json: true
         });
@@ -60,7 +58,7 @@ app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag) => {
     } else if (code) {
 
         let auth_user = await request({
-            uri: INSTAGRAM_URL + 'oauth/access_token/',
+            uri: INSTAGRAM_API + 'oauth/access_token/',
             method: 'POST',
             form: {
                 client_id: CLIENT_ID,
@@ -75,7 +73,7 @@ app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag) => {
         await client.set('access_token', auth_user.access_token);
 
         let photos = await request({
-            url: INSTAGRAM_URL + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
+            url: INSTAGRAM_API + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
             method: 'GET',
             json: true
         });
@@ -84,8 +82,7 @@ app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag) => {
 
     } else {
         let auth_params = {
-            instagram_url: INSTAGRAM_URL + 'oauth/authorize',
-            redirect_uri: REDIRECT_URI,
+            instagram_url: INSTAGRAM_API + 'oauth/authorize',
             client_id: CLIENT_ID,
             response_type: 'code',
             scope: 'public_content'
