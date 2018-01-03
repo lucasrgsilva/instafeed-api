647e5fae0fb9e7ec355980358426f0bf343c50df
Lucas Rodrigues Silva <lucas.silva@caseimagine.com>
2018-01-03 13:14:33 -0200

    .

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 
 const PORT = process.env.PORT || 5000
 // const REDIRECT_URI = 'http://localhost:5000/api/auth/done';
-const REDIRECT_URI = 'https://instagallery-api.herokuapp.com/api/auth/done';
+// const REDIRECT_URI = 'https://instagallery-api.herokuapp.com/api/auth/done';
 const INSTAGRAM_URL = 'https://api.instagram.com/';
 const CLIENT_ID = '20d1ab5af77445d9b09a46eaa6e3bb0c';
 const CLIENT_SECRET = 'add27729a01b41bead3d93da09581881';
@@ -36,34 +36,61 @@ app.use(route.get('/api/tag', async (ctx) => {
     ctx.body = { 'Message': 'No tag specified' };
 }));
 
-app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag ) => {
-    
+app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag) => {
+
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     tagVar = tag;
 
-    let access_token = await client.getAsync(user);
+    const access_token = await client.getAsync(user);
+    const code = ctx.request.query.code;
 
-    if (access_token) {
-        
-        let photos = await request({
-            url: INSTAGRAM_URL + `v1/tags/${tag}/media/recent?access_token=${access_token}`,
-            method: 'GET',
-            json: true
-        });
+    ctx.body = ctx.request;
+    // if (access_token) {
 
-        ctx.body = { message: 'Authenticated', images: photos.data };
+    //     let photos = await request({
+    //         url: INSTAGRAM_URL + `v1/tags/${tag}/media/recent?access_token=${access_token}`,
+    //         method: 'GET',
+    //         json: true
+    //     });
 
-    } else {
-        let auth_params = {
-            instagram_url: INSTAGRAM_URL + 'oauth/authorize',
-            redirect_uri: REDIRECT_URI,
-            client_id: CLIENT_ID,
-            response_type: 'code',
-            scope: 'public_content'
-        }
-        ctx.body = { message: 'Not authenticated', auth_params };
-    };
+    //     ctx.body = { message: 'Authenticated', images: photos.data };
+
+    // } else if (code) {
+
+    //     let auth_user = await request({
+    //         uri: INSTAGRAM_URL + 'oauth/access_token/',
+    //         method: 'POST',
+    //         form: {
+    //             client_id: CLIENT_ID,
+    //             client_secret: CLIENT_SECRET,
+    //             grant_type: 'authorization_code',
+    //             redirect_uri: 'ctx.request.href',
+    //             code: code
+    //         },
+    //         json: true
+    //     });
+
+    //     await client.set('access_token', auth_user.access_token);
+
+    //     let photos = await request({
+    //         url: INSTAGRAM_URL + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
+    //         method: 'GET',
+    //         json: true
+    //     });
+
+    //     ctx.body = { message: 'Authenticated', images: photos.data };
+
+    // } else {
+    //     let auth_params = {
+    //         instagram_url: INSTAGRAM_URL + 'oauth/authorize',
+    //         redirect_uri: REDIRECT_URI,
+    //         client_id: CLIENT_ID,
+    //         response_type: 'code',
+    //         scope: 'public_content'
+    //     }
+    //     ctx.body = { message: 'Not authenticated', auth_params };
+    // };
 
 }));
 
@@ -71,30 +98,7 @@ app.use(route.get('/api/auth/done', async (ctx, tag) => {
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    let params = ctx.request.query;
 
-    let auth_user = await request({
-        uri: INSTAGRAM_URL + 'oauth/access_token/',
-        method: 'POST',
-        form: {
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            grant_type: 'authorization_code',
-            redirect_uri: REDIRECT_URI,
-            code: params.code
-        },
-        json: true
-    });
-
-    await client.set('access_token', auth_user.access_token);
-
-    let photos = await request({
-        url: INSTAGRAM_URL + `v1/tags/${tagVar}/media/recent?access_token=${auth_user.access_token}`,
-        method: 'GET',
-        json: true
-    });
-
-    ctx.body = { message: 'Authenticated', images: photos.data };
 }));
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
