ed46a3ffb126d9ceb1255cba0c9ce088af1dd4bd
Lucas Rodrigues Silva <lucas.silva@caseimagine.com>
2018-01-03 10:15:47 -0200

    .

--- __index.js
+++ __index.js
@@ -12,8 +12,9 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 const PORT = process.env.PORT || 5000
+const REDIRECT_URI = 'http://localhost:4200/#/pages/gallery/feed';
 // const REDIRECT_URI = 'http://localhost:5000/api/auth/done';
-const REDIRECT_URI = 'https://instagallery-api.herokuapp.com/api/auth/done';
+// const REDIRECT_URI = 'https://instagallery-api.herokuapp.com/api/auth/done';
 const INSTAGRAM_URL = 'https://api.instagram.com/';
 const CLIENT_ID = '20d1ab5af77445d9b09a46eaa6e3bb0c';
 const CLIENT_SECRET = 'add27729a01b41bead3d93da09581881';
@@ -36,13 +37,13 @@ app.use(route.get('/api/tag', async (ctx) => {
     ctx.body = { 'Message': 'No tag specified' };
 }));
 
-app.use(route.get('/api/tag/:tag', async (ctx, tag) => {
+app.use(route.get('/api/tag/:user/:tag', async (ctx, user, tag ) => {
     
     ctx.set('Access-Control-Allow-Origin', '*');
     ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     tagVar = tag;
 
-    let access_token = await client.getAsync("access_token");
+    let access_token = await client.getAsync(user);
 
     if (access_token) {
         
@@ -52,12 +53,12 @@ app.use(route.get('/api/tag/:tag', async (ctx, tag) => {
             json: true
         });
 
-        ctx.body = { message: 'You are already authenticated', response: photos.data };
+        ctx.body = { message: 'Authenticated', images: photos.data };
 
     } else {
         let url = INSTAGRAM_URL + `oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=public_content`;
-        // ctx.body = { message: 'Not authenticated', redirectUrl: url };
-        ctx.redirect(url);
+        ctx.body = { message: 'Not authenticated', redirectUrl: url };
+        // ctx.redirect(url);
     };
 
 }));
